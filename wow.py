6566bd77d39fda598736e8d4bd73b3f8c48800fd
noahmiszewski-source <noah.miszewski@skola.taby.se>
2026-04-16 12:15:08 +0200

    highscore visning

--- wow.py
+++ wow.py
@@ -9,3 +9,20 @@ visa_meny()
 val = input("Val: ")
 print("Du valde:", val)
 
+def visa_highscore():
+    highscores = [
+        {"namn": "Erik", "gissningar": 2},
+        {"namn": "Anna", "gissningar": 3},
+        {"namn": "Sara", "gissningar": 5}
+    ]
+
+    if not highscores:
+        print("Inga resultat ännu")
+        return
+
+    # Sortera (minst antal gissningar först)
+    highscores.sort(key=lambda x: x["gissningar"])
+
+    print("--- HIGHSCORE ---")
+    for i, spelare in enumerate(highscores, start=1):
+        print(f"{i}. {spelare['namn']} - {spelare['gissningar']} gissningar")
