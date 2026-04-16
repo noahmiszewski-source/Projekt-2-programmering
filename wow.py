1abf6bd8246edc5e112eaee8533e2974b4070ec4
noahmiszewski-source <noah.miszewski@skola.taby.se>
2026-04-16 12:11:58 +0200

    Update wow.py

---
+++ wow.py
@@ -0,0 +1,11 @@
+def visa_meny():
+    print("--- HÖGT/LÅGT ---")
+    print("1. Spela ny omgång")
+    print("2. Visa highscore")
+    print("3. Avsluta")
+
+
+visa_meny()
+val = input("Val: ")
+print("Du valde:", val)
+
