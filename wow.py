4613c78b28711c9220ccac8dd109f486700bc10e
noahmiszewski-source <noah.miszewski@skola.taby.se>
2026-04-20 15:25:14 +0200

    Klar

--- wow.py
+++ wow.py
@@ -1,14 +1,31 @@
 import json
+import random
 
 def ladda_highscore():
     try:
         with open("data.json", "r") as f:
             return json.load(f)
-    
+    except FileNotFoundError:
+        return None
 
 def spara_highscore(data):
     with open("data.json", "w") as f:
         json.dump(data, f, indent=4)
+
+def spela():
+    hemligt_tal = random.randint(1, 100)
+    antal = 0
+    while True:
+        gissa = int(input("Gissa: "))
+        antal += 1
+        if gissa < hemligt_tal:
+            print("För lågt!")
+        elif gissa > hemligt_tal:
+            print("För högt!")
+        else:
+            print(f"Rätt! {antal} gissningar.")
+            return antal
+
 
 def visa_meny():
     print("--- HÖGT/LÅGT ---")
@@ -17,27 +34,43 @@ def visa_meny():
     print("3. Avsluta")
 
 
-visa_meny()
-val = input("Val: ")
-print("Du valde:", val)
-
 def visa_highscore():
-    highscores = [
-        {"namn": "Erik", "gissningar": 2},
-        {"namn": "Anna", "gissningar": 3},
-        {"namn": "Sara", "gissningar": 5}
-    ]
-
+    highscores = ladda_highscore()
     if not highscores:
         print("Inga resultat ännu")
         return
 
     # Sortera (minst antal gissningar först)
-    highscores.sort(key=lambda x: x["gissningar"])
-
+    sorterad = sorted(highscores, key=lambda x: x[" gissningar "])
     print("--- HIGHSCORE ---")
-    for i, spelare in enumerate(highscores, start=1):
-        print(f"{i}. {spelare['namn']} - {spelare['gissningar']} gissningar")
+    for spelare in sorterad:
+        print(f"{spelare[' namn ']} ({spelare[' gissningar ']})")
 
-data = ladda_highscore()
-print(data)
+
+def add_spelare(namn, antal):
+    spelare = {}
+    spelare[" namn "] = namn
+    spelare[" gissningar "] = antal
+    highscore = ladda_highscore()
+    if highscore is None:
+        highscore = []
+    highscore.append(spelare)
+    spara_highscore(highscore)
+
+
+while True:
+    visa_meny()
+    val = input("Val: ")
+    print("Du valde:", val)
+
+    if val == "1":
+        namn = input("Ange ditt namn: ")
+        antal = spela()
+        add_spelare(namn, antal)
+    elif val == "2":
+        visa_highscore()
+    elif val == "3":
+        print("Hejdå!")
+        break
+    else:
+        print("Ogiltigt val")
